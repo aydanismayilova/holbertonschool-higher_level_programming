6eff0fe1b97a9f708f697f10e5d208b7cf222c9e
aydanismayilova <ismayilovaaydan26@gmail.com>
2025-11-28 14:54:03 +0400

    Update 3-print_alphabt.py

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 for c in range(97, 123):
     if c != 101 and c != 113:
-        print("{}".format(chr(c)), end="")
-        
+        print("{:c}".format(c), end="")
