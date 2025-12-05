d405e8f7fcf88388cbc801a26992b38d82a95487
Aydan Ismayilova <ismayilovaaydan26@gmail.com>
2025-12-05 11:34:21 +0000

    Add 1-calculation.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 import calculator_1
 
-if __name__ == "__main__":
-    a = 10
-    b = 5
 
+def main(a, b):
+    """Perform all calculations with a and b and print results"""
     print("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
     print("{} - {} = {}".format(a, b, calculator_1.sub(a, b)))
     print("{} * {} = {}".format(a, b, calculator_1.mul(a, b)))
     print("{} / {} = {}".format(a, b, calculator_1.div(a, b)))
+
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    main(a, b)
