f06d13292ef2b37f84b75eeaa7d8c5b8ce0350cd
Aydan Ismayilova <ismayilovaaydan26@gmail.com>
2025-12-05 10:12:16 +0000

    Add 0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 from add_0 import add
 
-a = 1
-b = 2
 
-print("{} + {} = {}".format(a, b, add(a, b)))
+def fake_add(a, b):
+    """FAKE add function: performs subtraction instead of addition"""
+    return a - b
+
+
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, fake_add(a, b)))
