dae1123ad0d3ed676a32af362a480807e226d86c
Aydan Ismayilova <ismayilovaaydan26@gmail.com>
2025-12-06 21:26:30 +0000

    Add 7-add_tuple.py

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    # Normalize tuple_a
+    a1 = tuple_a[0] if len(tuple_a) > 0 else 0
+    a2 = tuple_a[1] if len(tuple_a) > 1 else 0
+
+    # Normalize tuple_b
+    b1 = tuple_b[0] if len(tuple_b) > 0 else 0
+    b2 = tuple_b[1] if len(tuple_b) > 1 else 0
+
+    return (a1 + b1, a2 + b2)
