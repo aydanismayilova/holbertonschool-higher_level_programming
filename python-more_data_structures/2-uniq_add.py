ea7d1a63f70169ee8652e33f5402a95eb6181e2b
Aydan Ismayilova <ismayilovaaydan26@gmail.com>
2025-12-12 13:50:12 +0000

    Fix 2-uniq_add.py

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,6 @@
-#!/usr/bin/python3                                                                                                              
+#!/usr/bin/python3
 def uniq_add(my_list=[]):
     total = 0
-    for i in my_list:
-        if my_list.count(i) == 1 or i not in my_list[:my_list.index(i)]:
-            total += i
+    for num in set(my_list):
+        total += num
     return total
