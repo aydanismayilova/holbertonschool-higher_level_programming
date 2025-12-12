5e08e01b0fb9054f678ed5c957ecee096d08fd29
Aydan Ismayilova <ismayilovaaydan26@gmail.com>
2025-12-12 13:47:14 +0000

    Fix 2-uniq_add.py

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python3                                                                                                              
 def uniq_add(my_list=[]):
     total = 0
     for i in my_list:
