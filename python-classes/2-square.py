3f630d0bb20e4567b75e29e5cbd4a5d4223b0641
aydanismayilova <ismayilovaaydan26@gmail.com>
2025-12-20 02:51:50 +0400

    Update 2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""Define a Square class with size validation"""
+"""Define a Square class"""
 
 
 class Square:
+    """Represents a square"""
+
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
