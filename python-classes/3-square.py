af38d596f6cbc0d5cdc894a92d30cd49a8db3324
aydanismayilova <ismayilovaaydan26@gmail.com>
2025-12-20 03:05:16 +0400

    Create 3-square.py

---
+++ python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Define a Square class with size validation and area computation"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
