a212f1c11e335c4df8b415b3984ae7404a99acdf
aydanismayilova <ismayilovaaydan26@gmail.com>
2025-12-21 23:41:57 +0400

    Update 7-rectangle.py

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -21,20 +21,3 @@ class Rectangle:
     def width(self):
         """Retrieve the width"""
         return self.__width
-
-    @width.setter
-    def width(self, value):
-        """Set the width with validation"""
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
-
-    @property
-    def height(self):
-        """Retrieve the height"""
-        return self.__height
-
-    @height.setter
-    def height(self, val
