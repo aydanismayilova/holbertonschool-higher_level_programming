401defcfb6032541714b98361f9e52154d431a7b
aydanismayilova <ismayilovaaydan26@gmail.com>
2025-12-21 23:41:09 +0400

    Update 7-rectangle.py

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -12,15 +12,29 @@ class Rectangle:
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle instance"""
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
 
     @property
     def width(self):
+        """Retrieve the width"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Set the width with validation"""
         if not isinstance(value, int):
-            raise TypeErro
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieve the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, val
