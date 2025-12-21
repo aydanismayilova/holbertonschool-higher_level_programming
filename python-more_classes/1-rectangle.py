c6de62b624c39c510d8083b80ce22a74c4e95747
aydanismayilova <ismayilovaaydan26@gmail.com>
2025-12-21 23:21:31 +0400

    Create  1-rectangle.py

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Rectangle sinfini təyin edir"""
+
+
+class Rectangle:
+    """Düzbucaqlı (Rectangle) sinfi"""
+
+    def __init__(self, width=0, height=0):
+        """Rectangle obyektini yaradır"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """width dəyərini qaytarır"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width dəyərini təyin edir"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """height dəyərini qaytarır""
