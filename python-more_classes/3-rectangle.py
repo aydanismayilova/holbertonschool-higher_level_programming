3222be253d3d50e46b819b22a6432664e92bd068
aydanismayilova <ismayilovaaydan26@gmail.com>
2025-12-21 23:46:42 +0400

    Update 3-rectangle.py

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -17,4 +17,39 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        """width dəyərin
+        """width dəyərini təyin edir"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """height dəyərini qaytarır"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height dəyərini təyin edir"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Düzbucaqlının sahəsini qaytarır"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Düzbucaqlının perimetrini qaytarır"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Düzbucaqlını # işarəsi ilə çap edir"""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        return "
