f94a5ba429434d478d10651f011350d27f91666e
aydanismayilova <ismayilovaaydan26@gmail.com>
2026-02-11 01:21:50 +0400

    Update 3-value_argument.js

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,12 +1,7 @@
 #!/usr/bin/node
-
-let count = 0;
-process.argv.forEach(() => {
-  count++;
-});
-
-if (count === 2 || count < 2) {
-  console.log('No argument');
-} else {
-  console.log(process.argv[2]);
+const firstArg = process.argv[2];
+if(firstArg===undefined){
+    console.log("No Argument");
+}else{
+    console.log(firstArg);
 }
