0be02bbe9554ece716c4e20088d5ef6f2f2da902
aydanismayilova <ismayilovaaydan26@gmail.com>
2026-02-11 01:22:41 +0400

    Update 3-value_argument.js

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const firstArg = process.argv[2];
-if(firstArg===undefined){
+if (firstArg===undefined){
     console.log("No Argument");
-}else{
+} else {
     console.log(firstArg);
 }
